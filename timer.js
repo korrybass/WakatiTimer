dfca11046ac1bdbb6ad83230e2fe8f555d7b021e
korrybass <korry@penheadcreative.com>
2017-02-28 00:17:07 -0500

    added countdown

--- timer.js
+++ timer.js
@@ -1,6 +1,7 @@
 var _getRemainingTime = function (time) {
     var t = time;
     var seconds = Math.floor( (t/1000) % 60 );
+    
     var minutes = Math.floor( (t/1000/60) % 60 );
     return {
         seconds: seconds,
@@ -20,11 +21,17 @@ var _countDown = function (time) {
 }
 
 var updateElement = function (updatedTime) {
+    var secondsString = updatedTime.seconds.toString();
+    var secondsArr = secondsString.split("");
+    if(secondsArr.length === 1){ 
+        secondsArr.unshift("0");
+        secondsString = secondsArr.join("");
+    }
     var wakatiElem = document.querySelector('#wakati');
     var WKseconds = wakatiElem.querySelector('.seconds');
     var WKminutes = wakatiElem.querySelector('.minutes'); 
     WKminutes.innerHTML = updatedTime.minutes.toString();
-    WKseconds.innerHTML = updatedTime.seconds.toString();
+    WKseconds.innerHTML = secondsString;
 }
 
 var WakatiFactory = function () {
